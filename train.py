b62d8338a1ca3c5a200b20e277e0f5aaf65d99c3
SFauth <72087109+SFauth@users.noreply.github.com>
2022-11-25 04:28:52 +0900

    Update train.py

--- train.py
+++ train.py
@@ -14,8 +14,11 @@ if __name__ == '__main__':
 
     os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
     os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-
-    parser = argparse.ArgumentParser(description='Settings.')
+    
+    # when running code in the command line, parser makes the command line interactive:
+    # if we then type "train.py --help" we get a summary of our options 
+    
+    parser = argparse.ArgumentParser(description='Settings.')  
     parser.add_argument('-n', '--exp_name', default='exp_name', type=str,
                         help='Name of the experiment.')
     parser.add_argument('-d', '--dataset', default='Clotho', type=str,
